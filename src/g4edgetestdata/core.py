8ed676df0e0e5cf47ce3f9b7bc19efd065002a14
Laurie Nevay <laurie.nevay@cern.ch>
2025-01-04 11:09:38 +0100

    Build a list of files available dynamically. Docstring for class.

--- src/g4edgetestdata/core.py
+++ src/g4edgetestdata/core.py
@@ -12,12 +12,26 @@ log = logging.getLogger(__name__)
 
 
 class G4EdgeTestData:
+    """
+    Class to access all test data. Data can be accessed via the path using the
+    [] operator. A full list of available files (built dynamically) is given in
+    the member `files`.
+
+    >>> d = G4EdgeTestData()
+    >>> d.files
+        ['convert/T001_geant4Box2Fluka.gdml',
+         'convert/T001_geant4Box2Fluka.inp',
+         'convert/T001_geant4Box2Fluka_baked.inp',
+        ...
+    >>> abs_path = d['convert/T001_geant4Box2Fluka.inp']
+    """
     def __init__(self):
         self._default_git_ref = "main"
         self._repo_path = Path(
             os.getenv("G4EDGE_TESTDATA", gettempdir() + "/g4edge-testdata-" + getuser())
         )
         self._repo: Repo = self._init_testdata_repo()
+        self._build_list_of_available_data()
 
     def _init_testdata_repo(self) -> None:
         if not self._repo_path.is_dir():
@@ -64,3 +78,17 @@ class G4EdgeTestData:
             raise FileNotFoundError(msg)
 
         return full_path
+
+    def _build_list_of_available_data(self):
+        """
+        Build a list of all available data dynamically. From python 3.12 we could use
+        `Path.walk`, but we use down to 3.7, therefore use the `os.walk` method instead.
+        """
+        self.files = []
+        root = Path(self._repo_path / "data")
+        for dirpath, dirnames, filenames in os.walk(root):
+            for f in filenames:
+                common = os.path.relpath(dirpath, root)
+                rp = os.path.join(common, f)
+                self.files.append(rp)
+        self.files = sorted(self.files)
